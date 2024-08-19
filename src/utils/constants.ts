c42ceb80861e6164f278c53b951fa9b67916fe47
Eduard <dumpweed10@gmail.com>
2024-08-19 12:55:48 +0300

    update for gh pages

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,7 @@
-export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
-export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
+const API_ORIGIN = 'https://larek-api.nomoreparties.co'
+
+export const API_URL = `${API_ORIGIN}/api/weblarek`;
+export const CDN_URL = `${API_ORIGIN}/content/weblarek`;
 
 export const settings = {
 	headers: {
